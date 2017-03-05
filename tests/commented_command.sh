53cd11f2c26315c361291acdb5b4f791cf81a718
Jeremy O'Neill <jonei005@ucr.edu>
2017-03-04 20:28:22 -0800

    updated tests

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -11,8 +11,14 @@ ls #-l
 ls # -l
 echo hello # world
 echo hello world && ls # -l
-echo hello world && #
+echo hello world &&#
+echo u && echo b  #############a 
+echo tests #uausuau echo tests
+echo a 
+(echo a) || echo b# echo asdasdasdasdasd
+(echo a || (echo b && echo c))
 #echo hello world
 # echo hello world
+echo a # echo b 
 exit
 EOF
